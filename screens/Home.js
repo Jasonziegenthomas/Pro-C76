4cca371a3e4a4545a7418bca99fe13b9dd551fa1
Jasonziegenthomas <73540080+Jasonziegenthomas@users.noreply.github.com>
2021-10-07 10:45:30 +0530

    Add files via upload

---
+++ screens/Home.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './Screens/Home';
+import SpaceCraft from './Screens/SpaceCraft';
+import DailyPic from './Screens/DailyPic';
+
+export default class HomeScreen extends Component {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                <Text>Home Screen!</Text>
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
